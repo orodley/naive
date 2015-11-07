c6dfcaaad2d6815e41af4b5f2dbdb92d5733574a
Owen Rodley <owen.rodley@gmail.com>
2015-11-07 23:59:11 +1300

    Memory map the input file.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,45 @@
-int main(void)
+#include <stdio.h>
+#include "misc.h"
+
+// @PORT
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h> 
+
+// @PORT
+char *map_file_into_memory(const char *filename)
 {
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return NULL;
+
+	off_t file_size = lseek(fd, 0, SEEK_END);
+
+	if (file_size == -1)
+		return NULL;
+
+	char *buffer = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+	if (buffer == MAP_FAILED)
+		return NULL;
+
+	return buffer;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <input file>\n", argv[0]);
+		return 1;
+	}
+
+	char *input_filename = argv[1];
+	char *buffer = map_file_into_memory(input_filename);
+
+	if (buffer == NULL)
+		return 1;
+
+	printf("char 1 = '%c'\n", buffer[0]);
+
 	return 0;
 }
---
+++ src/misc.h
@@ -0,0 +1,20 @@
+// Include this everywhere for types and stuff
+
+#ifndef NAIVE_MISC_H_
+#define NAIVE_MISC_H_
+
+#include <stdint.h>
+
+typedef uint8_t  u8;
+typedef  int8_t  i8;
+typedef uint16_t u16;
+typedef  int16_t i16;
+typedef uint32_t u32;
+typedef  int32_t i32;
+typedef uint64_t u64;
+typedef  int64_t i64;
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#endif
