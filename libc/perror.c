2b32578246d613855ea2b81acee4552cc61dffa6
Owen Rodley <owen.rodley@gmail.com>
2017-03-05 20:22:48 +1100

    Add a hack in perror to compensate for our linker.

--- libc/perror.c
+++ libc/perror.c
@@ -1,9 +1,14 @@
 #include <assert.h>
 #include <stdio.h>
 
+// @NOTE: This is a hack to force the compiler not to generate a .rodata
+// section which our linker can't handle right now. If it were an inline string
+// literal, it would be put in .rodata
+static char fmt[] = "%s: something went wrong!";
+
 void perror(const char *s)
 {
 	// @TODO: Implement this properly.
-	int ret = printf("%s: something went wrong!", s);
+	int ret = printf(fmt, s);
 	assert(ret > 0);
 }
