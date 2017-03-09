10036a37ed67da51b02dd7c371409e1a0906c412
Owen Rodley <owen.rodley@gmail.com>
2017-03-09 20:48:51 +1100

    Make perror a little more useful.

--- libc/perror.c
+++ libc/perror.c
@@ -1,14 +1,15 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 
 // @NOTE: This is a hack to force the compiler not to generate a .rodata
 // section which our linker can't handle right now. If it were an inline string
 // literal, it would be put in .rodata
-static char fmt[] = "%s: something went wrong!";
+static char fmt[] = "%s: something went wrong! (errno = %d)\n";
 
 void perror(const char *s)
 {
 	// @TODO: Implement this properly.
-	int ret = printf(fmt, s);
+	int ret = printf(fmt, s, errno);
 	assert(ret > 0);
 }
