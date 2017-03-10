1902210e9f06fd92e18c804f0d21c3669fba208d
Owen Rodley <owen.rodley@gmail.com>
2017-03-10 22:06:53 +1100

    Implement memmove.

--- libc/include/string.h
+++ libc/include/string.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 
 void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 size_t strlen(const char *s);
 
---
+++ libc/memmove.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+
+void *memmove(void *_dest, const void *_src, size_t n)
+{
+	unsigned char *dest = (unsigned char *)_dest;
+	unsigned char *src = (unsigned char *)_src;
+
+	unsigned char *dest_end = dest + n;
+	unsigned char *src_end = src + n;
+
+	if (dest_end > src && dest_end < src_end) {
+		// dest   |-----|
+		// src      |-----|
+		// Copy one-by-one from the start to avoid overwriting.
+		for (size_t i = 0; i < n; i++) {
+			dest[i] = src[i];
+		}
+	} else if (src_end > dest && src_end < dest_end) {
+		// dest   |-----|
+		// src  |-----|
+		// Copy one-by-one from the end to avoid overwriting.
+		for (ssize_t i = n - 1; i >= 0; i--) {
+			dest[i] = src[i];
+		}
+	} else if (dest != src) {
+		memcpy(dest, src, n);
+	}
+
+	return dest;
+}
