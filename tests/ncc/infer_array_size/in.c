b111823ba544e054a49bf57d49ad394e32f1d88e
Owen Rodley <owen.rodley@gmail.com>
2017-04-20 18:49:34 +1000

    Add test for inferring array size with string literals.

--- tests/ncc/infer_array_size/in.c
+++ tests/ncc/infer_array_size/in.c
@@ -5,9 +5,11 @@ int foo[] = { 1, 2, 3, 4 };
 int main()
 {
 	int bar[] = { 4, 3, 2 };
+	char baz[] = "quux";
 
-	assert(sizeof(foo) / sizeof(*foo) == 4);
-	assert(sizeof(bar) / sizeof(*bar) == 3);
+	assert(sizeof foo / sizeof *foo == 4);
+	assert(sizeof bar / sizeof *bar == 3);
+	assert(sizeof baz / sizeof *baz == 4);
 
 	assert(foo[0] == 1);
 	assert(foo[1] == 2);
@@ -17,6 +19,11 @@ int main()
 	assert(bar[0] == 4);
 	assert(bar[1] == 3);
 	assert(bar[2] == 2);
+
+	assert(baz[0] == 'q');
+	assert(baz[1] == 'u');
+	assert(baz[2] == 'u');
+	assert(baz[3] == 'x');
 
 	return 0;
 }
