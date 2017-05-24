e388668c89fa3388ad6fae276e49bd5143ea5e5b
Owen Rodley <owen.rodley@gmail.com>
2017-05-24 21:26:33 +1000

    Add test for preceding three commits.

---
+++ tests/ncc/changed_incoming_block/in.c
@@ -0,0 +1,11 @@
+#include <assert.h>
+
+int main()
+{
+	int a = 1, b = 1, c = 1, d = 1;
+	assert((a || b) || (c || d));
+	assert((a && b) && (c && d));
+	assert((a ? b : c) ? (b ? c : d) : (c ? d : a));
+
+	return 0;
+}
