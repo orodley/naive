5096df8418a34f55c40561bc0e5a83386d0d1139
Owen Rodley <owen.rodley@gmail.com>
2017-06-13 19:29:49 +1000

    Add tests relating to the previous few commits.

---
+++ tests/ncc/local_nested_array/in.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+
+int main()
+{
+	int foo[][2] = {{1, 2}, {3, 4}};
+	assert(foo[0][0] == 1);
+	assert(foo[0][1] == 2);
+	assert(foo[1][0] == 3);
+	assert(foo[1][1] == 4);
+
+	return 0;
+}
---
+++ tests/ncc/nested_array_of_string_literals/in.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+
+char *foo[][2] = {{"a", "b"}, {"c", "d"}};
+
+int main()
+{
+	assert(foo[0][0][0] == 'a');
+	assert(foo[0][0][1] == '\0');
+	assert(foo[0][1][0] == 'b');
+	assert(foo[0][1][1] == '\0');
+	assert(foo[1][0][0] == 'c');
+	assert(foo[1][0][1] == '\0');
+	assert(foo[1][1][0] == 'd');
+	assert(foo[1][1][1] == '\0');
+
+	return 0;
+}
