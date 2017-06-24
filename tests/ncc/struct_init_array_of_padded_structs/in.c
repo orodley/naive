df52622b317e55b88c6f9d62e0273defa0164a35
Owen Rodley <owen.rodley@gmail.com>
2017-06-24 14:23:37 +1000

    Add a similar test to the previous one, with arrays this time.

---
+++ tests/ncc/struct_init_array_of_padded_structs/in.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+
+int main()
+{
+	struct
+	{
+		struct { int *a; int b; } a[2];
+	} b = { .a[1].b = 1 };
+	assert(sizeof b.a == 32);
+	assert(sizeof b.a[0] == 16);
+	assert(b.a[1].b == 1);
+
+	return 0;
+}
