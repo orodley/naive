7d9de9c2513bbd3341d7cb788f15a30d91ee0ab4
Owen Rodley <owen.rodley@gmail.com>
2024-07-09 00:48:37 +1200

    Fix missing casts in readlink

--- libc/readlink.c
+++ libc/readlink.c
@@ -4,5 +4,5 @@
 
 ssize_t readlink(const char *restrict path, char *restrict buf, size_t size)
 {
-  return __syscall(89, path, buf, size, 0, 0, 0);
+  return __syscall(89, (uint64_t)path, (uint64_t)buf, size, 0, 0, 0);
 }
